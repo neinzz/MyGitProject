e822e86ebc72afd19b0abb960644de0b793d03a6
neinzz <heins.nicholas@gmail.com>
2017-09-11 21:04:45 -0700

    Initial commit

---
+++ Source/MyGitProject/MyGitProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGitProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGITPROJECT_API AMyGitProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
